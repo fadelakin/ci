bd1d3057e6be7ccbe70019704f68a7e9c932fdef
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-03-19 15:33:35 -0400

    fragments

--- app/src/main/java/com/fisheradelakin/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/fisheradelakin/criminalintent/CrimeActivity.java
@@ -1,32 +1,23 @@
 package com.fisheradelakin.criminalintent;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        FragmentManager fm  = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
