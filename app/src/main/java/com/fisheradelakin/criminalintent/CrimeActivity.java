b67db3bf9917378ed1f027fd55aff391fc7a5c6e
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-03-20 20:11:59 -0400

    toolbar

--- app/src/main/java/com/fisheradelakin/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/fisheradelakin/criminalintent/CrimeActivity.java
@@ -1,16 +1,21 @@
 package com.fisheradelakin.criminalintent;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
-public class CrimeActivity extends Activity {
+public class CrimeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
 
         FragmentManager fm  = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
--- app/src/main/java/com/fisheradelakin/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/fisheradelakin/criminalintent/CrimeFragment.java
@@ -21,6 +21,8 @@ public class CrimeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
         mCrime = new Crime();
     }
 
